6c3d20d84f5e7b9743f229b40493f0c3859f0bac
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2023-10-30 17:06:29 -0400

    Added code for ending the quiz and showing results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,78 @@
 var initialElements = document.querySelector("#initial");
 var quizElements = document.querySelector("#quiz");
+var resultElements = document.querySelector("#results");
+
+var timer = document.querySelector("#timer");
 
 var startButton = document.querySelector("#start-button");
-startButton.addEventListener("click", startQuiz);
+startButton.addEventListener("click", takeQuiz);
 
+var secondsLeft = 5;
+var score = 0;
 
+function takeQuiz() {
+  renderQuiz();
+  startTimer();
 
-var q1 = "How do you write comments in HTML?";
-var q1Answers = ["<!--Comment-->", "//Comment", "/*Comment*/", "<Comment>"];
+  // Quiz logic goes here
+}
 
-var q2 = "What is the command for changing text color to blue in CSS?";
-var q2Answers = ["text-color: blue", "color: blue", "color -> blue", "tc: blue"];
-
-var q3 = "What is the keyword for declaring a variable in JavaScript?";
-var q3Answers = ["variable", "data", "var", "declare"];
-
-
-
-function startQuiz() {
+function renderQuiz() {
   // Remove all child elements from the "initial" div of the HTML
   while (initialElements.firstChild) {
     initialElements.removeChild(initialElements.firstChild);
   }
+  // Render quiz elements
+  var question = document.createElement("p");
+  quizElements.appendChild(question);
 
-  // Append timer asset to HTML
-  var timerAsset = document.createElement("p");
-  var secondsLeft = 10;
-  quizElements.appendChild(timerAsset);
-  timerAsset.textContent = "Time remaining: " + secondsLeft;
+  var answer1 = document.createElement("button");
+  var answer2 = document.createElement("button");
+  var answer3 = document.createElement("button");
+  var answer4 = document.createElement("button");
+  quizElements.appendChild(answer1);
+  quizElements.appendChild(answer2);
+  quizElements.appendChild(answer3);
+  quizElements.appendChild(answer4);
 
+  question.textContent = "Question goes here";
+  answer1.textContent = "Answer 1";
+  answer2.textContent = "Answer 2";
+  answer3.textContent = "Answer 3";
+  answer4.textContent = "Answer 4";
+}
 
-  // Start timer
+function startTimer() {
+  timer.textContent = "Time remaining: " + secondsLeft;
+
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timerAsset.textContent = "Time remaining: " + secondsLeft;
+    timer.textContent = "Time remaining: " + secondsLeft;
 
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
+
+      showResults();
     }
   }, 1000);
 }
+
+function showResults() {
+  console.log("showResults is being called");
+
+  // Remove quiz elements
+  while (quizElements.firstChild) {
+    quizElements.removeChild(quizElements.firstChild);
+  }
+  // Render score elements
+}
+
+// var q1 = "How do you write comments in HTML?";
+// var q1Answers = ["<!--Comment-->", "//Comment", "/*Comment*/", "<Comment>"];
+
+// var q2 = "What is the command for changing text color to blue in CSS?";
+// var q2Answers = ["text-color: blue", "color: blue", "color -> blue", "tc: blue"];
+
+// var q3 = "What is the keyword for declaring a variable in JavaScript?";
+// var q3Answers = ["variable", "data", "var", "declare"];
