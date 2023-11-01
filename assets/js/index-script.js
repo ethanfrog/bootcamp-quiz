14d429125476091f3ddac7269d1a60aa2e79bcd1
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2023-10-31 20:41:32 -0400

    JavaScript code refactoring

--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -1,35 +1,30 @@
-var initialElements = document.querySelector("#initial");
-var quizElements = document.querySelector("#quiz");
-var resultElements = document.querySelector("#results");
-
+// The timer display at the top of the page
 var timer = document.querySelector("#timer");
+var secondsLeft = 100;
 
+// The quiz elements that are initially shown when the page is loaded
+var initialElements = document.querySelector("#initial");
 var startButton = document.querySelector("#start-button");
+// Start the quiz upon clicking the start button
 startButton.addEventListener("click", takeQuiz);
 
-var secondsLeft = 100;
+// The elements that make up the actual quiz, only created once a quiz is started
+var quizElements = document.querySelector("#quiz");
+// The test takers score value
 var score = 0;
-
+// The current question's text display
 var questionText = document.createElement("p");
-
+// The answers to the current question
 var answerButtons = document.createElement("div");
 var answer1 = document.createElement("button");
 var answer2 = document.createElement("button");
 var answer3 = document.createElement("button");
 var answer4 = document.createElement("button");
-
+// Text describing the result of the previous question (correct/wrong)
 var questionResult = document.createElement("p");
 
-var scoreText = document.createElement("p");
-var initialsPrompt = document.createElement("p");
-var initialsInput = document.createElement("input");
-var submitButton = document.createElement("button");
-
-var studentResults = {
-  stuName: "",
-  stuScore: 0
-}
-
+// An array of quiz questions. Each contains the text of each question, the
+// text of its possible answers, and data on which answer is correct
 var questions = [
   {
     qText: "How do you write comments in HTML?",
@@ -56,11 +51,27 @@ var questions = [
     correct: 3
   }
 ]
-
+// The current question being displayed
 var currentQuestionIndex = 0;
 
+// The elements that make up quiz results, only created once a quiz is finished
+var resultElements = document.querySelector("#results");
+// A text display for the quiz taker's final score
+var scoreText = document.createElement("p");
+// Elements for inputting initials and sending them to local storage
+var initialsPrompt = document.createElement("p");
+var initialsInput = document.createElement("input");
+var submitButton = document.createElement("button");
+
+// An object for storing quiz results, to be placed into local storage
+var studentResults = {
+  stuName: "",
+  stuScore: 0
+}
 
 
+
+// Starts the quiz by enabling the timer, displaying quiz elements and injecting the first question's content
 function takeQuiz() {
   startTimer();
   renderQuiz();
@@ -68,12 +79,50 @@ function takeQuiz() {
   setNextQuestion(questions[currentQuestionIndex]);
 }
 
+function startTimer() {
+  // Initialize the timer display
+  timer.textContent = "Time remaining: " + secondsLeft;
+
+  var timerInterval = setInterval(function() {
+    // Update the timer display every second
+    secondsLeft--;
+    timer.textContent = "Time remaining: " + secondsLeft;
+
+    // End quiz when time runs out
+    if(secondsLeft <= 0) {
+      timer.textContent = "Time remaining: 0";
+      clearInterval(timerInterval);
+      showResults();
+    }
+  }, 1000); //1000 milliseconds = 1 second
+}
+
+function renderQuiz() {
+  // Remove all child elements from the "initial" div of the HTML
+  while (initialElements.firstChild) {
+    initialElements.removeChild(initialElements.firstChild);
+  }
+
+  // Render quiz elements
+  quizElements.appendChild(questionText);
+
+  quizElements.appendChild(answerButtons);
+  answerButtons.appendChild(answer1);
+  answerButtons.appendChild(answer2);
+  answerButtons.appendChild(answer3);
+  answerButtons.appendChild(answer4);
+
+  quizElements.appendChild(questionResult);
+}
+
 function setNextQuestion(question) {
+  // Clear the correct status from all answers
   answer1.classList.remove("correct");
   answer2.classList.remove("correct");
   answer3.classList.remove("correct");
   answer4.classList.remove("correct");
 
+  // Give one answer the correct status based on question data
   if (question.correct === 1) {
     answer1.setAttribute("class", "correct");
   }
@@ -87,23 +136,25 @@ function setNextQuestion(question) {
     answer4.setAttribute("class", "correct");
   }
 
+  // Fill out quiz elements with the content of the question parameter
   questionText.textContent = question.qText;
   answer1.textContent = question.a1;
   answer2.textContent = question.a2;
   answer3.textContent = question.a3;
   answer4.textContent = question.a4;
-
+  // Process the user's answer when an answer button is clicked
   answerButtons.addEventListener("click", checkAnswer)
 }
 
 function checkAnswer(event) {
-  // Check if selected answer was right, act accordingly
+  // Get the selected button
   var selectedButton = event.target;
-
+  // If the selected button has the correct status, increase score
   if (selectedButton.classList.contains("correct")) {
     questionResult.textContent = "Correct!";
     score++;
   }
+  // Otherwise, decrement time remaining
   else {
     questionResult.textContent = "Wrong!";
     if (secondsLeft < 10) {
@@ -114,7 +165,7 @@ function checkAnswer(event) {
     }
   }
 
-  // Move to next question, or end quiz
+  // Move on to next question, or end the quiz if there are no questions remaining
   if (currentQuestionIndex < questions.length - 1) {
     currentQuestionIndex++;
     setNextQuestion(questions[currentQuestionIndex]);
@@ -124,46 +175,9 @@ function checkAnswer(event) {
   }
 }
 
-
-
-function renderQuiz() {
-  // Remove all child elements from the "initial" div of the HTML
-  while (initialElements.firstChild) {
-    initialElements.removeChild(initialElements.firstChild);
-  }
-
-  // Render quiz elements
-  quizElements.appendChild(questionText);
-
-  quizElements.appendChild(answerButtons);
-
-  answerButtons.appendChild(answer1);
-  answerButtons.appendChild(answer2);
-  answerButtons.appendChild(answer3);
-  answerButtons.appendChild(answer4);
-
-  quizElements.appendChild(questionResult);
-}
-
-function startTimer() {
-  timer.textContent = "Time remaining: " + secondsLeft;
-
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timer.textContent = "Time remaining: " + secondsLeft;
-
-    if(secondsLeft <= 0) {
-      timer.textContent = "Time remaining: 0";
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-
-      showResults();
-    }
-  }, 1000);
-}
-
 function showResults() {
   secondsLeft = 0;
+
   // Remove quiz elements
   while (quizElements.firstChild) {
     quizElements.removeChild(quizElements.firstChild);
@@ -179,6 +193,7 @@ function showResults() {
   initialsPrompt.textContent = "Enter initials:";
   submitButton.textContent = "Submit";
 
+  // Upload results to local storage when the submit button is clicked
   submitButton.addEventListener("click", function(){
     // Update student results object, only if initials were put in
     if (initialsInput.value !== "") {
@@ -188,6 +203,7 @@ function showResults() {
       // Save results to local storage, using initials as a key
       localStorage.setItem(studentResults.stuName, JSON.stringify(studentResults.stuScore));
 
+      // Clear the input bar after submission
       initialsInput.value = "";
     }
   })
--- assets/js/scores-script.js
+++ assets/js/scores-script.js
@@ -1,11 +1,14 @@
+// The section of the page that will display saved scores
 var scoreElements = document.querySelector("#scores");
 
+// The name and score of a student that has taken the quiz
 var stuName;
 var stuScore;
 
+// An HTML element that scores will be placed into
 var newScore;
 
-//Iterate through local storage, display saved scores
+//Iterate through local storage and display its content
 for (i = 0; i < localStorage.length; i++) {
   stuName = localStorage.key(i);
   stuScore = JSON.parse(localStorage.getItem(localStorage.key(i)));
